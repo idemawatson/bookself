d106befa03bd83840f1d3928e5f27b388538f27f
井手拓海 <idetakumi@idems.local>
2023-05-03 17:06:34 +0900

    pwa対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 import { LoginButton, LogoutButton } from "@/components/AuthButtons";
+import Head from "./Head";
 
 export default function Home() {
   return (
-    <main
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "70vh",
-      }}
-    >
-      <div>
-        <LoginButton />
-        <LogoutButton />
-      </div>
-    </main>
+    <>
+      <Head />
+      <body>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500&display=swap"
+          rel="stylesheet"
+        />
+        <main
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "70vh",
+          }}
+        >
+          <LoginButton />
+          <LogoutButton />
+        </main>
+      </body>
+    </>
   );
 }
