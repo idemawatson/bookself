978378926f20b87df2172ba829e302fdcb0dac83
井手拓海 <idetakumi@idems.local>
2023-05-20 17:55:04 +0900

    favicon追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,16 +8,13 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500&display=swap'
           rel='stylesheet'
         />
-        <meta name='application-name' content='PWA App' />
+        <meta name='application-name' content='BookSelf' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-        <meta name='apple-mobile-web-app-title' content='PWA App' />
-        <meta name='description' content='Best PWA App in the world' />
+        <meta name='apple-mobile-web-app-title' content='BookSelf' />
+        <meta name='description' content='Manage Books by Yourself.' />
         <meta name='format-detection' content='telephone=no' />
         <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='msapplication-config' content='/icons/browserconfig.xml' />
-        <meta name='msapplication-TileColor' content='#2B5797' />
-        <meta name='msapplication-tap-highlight' content='no' />
         <meta name='theme-color' content='#000000' />
         <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
         <link
@@ -38,14 +35,8 @@ export default function Document() {
         <link
           rel='icon'
           type='image/png'
-          sizes='32x32'
-          href='/icons/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
           sizes='16x16'
-          href='/icons/favicon-16x16.png'
+          href='/icon-16x16.png'
         />
         <link rel='manifest' href='/manifest.json' />
         <link
@@ -54,24 +45,6 @@ export default function Document() {
           color='#5bbad5'
         />
         <link rel='shortcut icon' href='/favicon.ico' />
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:url' content='https://yourdomain.com' />
-        <meta name='twitter:title' content='PWA App' />
-        <meta name='twitter:description' content='Best PWA App in the world' />
-        <meta
-          name='twitter:image'
-          content='https://yourdomain.com/icons/android-chrome-192x192.png'
-        />
-        <meta name='twitter:creator' content='@DavidWShadow' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='PWA App' />
-        <meta property='og:description' content='Best PWA App in the world' />
-        <meta property='og:site_name' content='PWA App' />
-        <meta property='og:url' content='https://yourdomain.com' />
-        <meta
-          property='og:image'
-          content='https://yourdomain.com/icons/apple-touch-icon.png'
-        />
       </Head>
       <body>
         <Main />
