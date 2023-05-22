f6041cfc8313d57c6cb926821ac84efb832bdf9e
井手拓海 <idetakumi@idems.local>
2023-05-23 00:33:16 +0900

    レートグラフからyearパラメータを削除

--- src/controllers/GetBookRatioController.ts
+++ src/controllers/GetBookRatioController.ts
@@ -9,25 +9,12 @@ type RawData = {
 }[]
 
 export default class GetBookRatioController {
-  async execute({
-    user_id,
-    year,
-  }: {
-    user_id?: string
-    year: number
-  }): Promise<BookRatioResponse> {
+  async execute({ user_id }: { user_id?: string }): Promise<BookRatioResponse> {
     if (!user_id) throw new ValidationError('user_id required')
-    if (!Number.isInteger(year)) throw new ValidationError('year is invalid')
 
-    const gte = dayjs().year(year).startOf('year').toDate()
-    const lt = dayjs().year(year).endOf('year').toDate()
     const data = await prisma.book.groupBy({
       where: {
         user_id,
-        created_at: {
-          gte,
-          lt,
-        },
       },
       by: ['status'],
       _count: true,
--- src/pages/api/ratio/index.ts
+++ src/pages/api/ratio/index.ts
@@ -9,10 +9,8 @@ const getHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerSession(req, res, authOptions)
 
   const controller = new GetBookRatioController()
-  const { year } = req.query
   const books = await controller.execute({
     user_id: session?.user.id,
-    year: Number(year),
   })
   res.json(books)
 }
